86b6dc5d5c9e8a92e333fcf94be8bae2b79ba862
Elliott <s4922057@bournemouth.ac.uk>
2018-05-01 15:28:26 +0100

    Updated Text again.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Test Change
+                    Laravel Test Change Again
                 </div>
 
                 <div class="links">
