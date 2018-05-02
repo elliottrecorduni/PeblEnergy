13c61210d81c3d9b5ddba06998775005ff3d4346
Unknown <s4922057@bournemouth.ac.uk>
2018-05-02 11:58:48 +0100

    Updated main page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Branch Change
+                    Laravel Test Change.
                 </div>
 
                 <div class="links">
