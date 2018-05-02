2186781a34a6db7e55734c79ac68586ae0046259
Unknown <s4922057@bournemouth.ac.uk>
2018-05-02 12:04:45 +0100

    Update to main page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Branch Change
+                    Laravel Branch Change another change.
                 </div>
 
                 <div class="links">
