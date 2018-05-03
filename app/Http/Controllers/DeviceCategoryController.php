ef6a13bc94882b11594406eb6ca08b8ccb685f91
Elliott <s4922057@bournemouth.ac.uk>
2018-05-03 10:51:14 +0100

    Added category controller and model.

---
+++ app/Http/Controllers/DeviceCategoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DeviceCategory;
+use Illuminate\Http\Request;
+
+class DeviceCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\DeviceCategory  $deviceCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DeviceCategory $deviceCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\DeviceCategory  $deviceCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DeviceCategory $deviceCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\DeviceCategory  $deviceCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DeviceCategory $deviceCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\DeviceCategory  $deviceCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DeviceCategory $deviceCategory)
+    {
+        //
+    }
+}
