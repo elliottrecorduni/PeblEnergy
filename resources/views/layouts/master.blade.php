5fcb1bd2a4ecb576c1e4c0f7bcaedac0aba336bf
Jack Hayward <imjackhayward@gmail.com>
2018-05-04 10:42:15 +0100

    Created UserController (resource), added the resource route and created the show functionality within the controller

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,10 +15,10 @@
         <div class="sidebar navbar-light bg-light pt-2 h-1000">
             <div class="col-2 collapse d-md-flex navbar-light bg-light pt-2 h-1000" id="sidebar" style="max-width:140px;"> <!-- collapse -->
                 <ul class="nav flex-column">
-                    <li class="nav-item"><a class="nav-link" href="index.html"><i class="fas fa-home text-dark"></i>Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="budget.html"><i class="fas fa-money-bill-alt text-dark"></i>Budget</a></li>
-                    <li class="nav-item"><a class="nav-link" href="devices.html"><i class="fas fa-mobile-alt text-dark"></i>Devices</a></li>
-                    <li class="nav-item"><a class="nav-link" href="settings.html"><i class="fas fa-cog text-dark"></i>Settings</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.html"><i class="fas fa-home text-dark"></i> Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="budget.html"><i class="fas fa-money-bill-alt text-dark"></i> Budget</a></li>
+                    <li class="nav-item"><a class="nav-link" href="devices.html"><i class="fas fa-mobile-alt text-dark"></i> Devices</a></li>
+                    <li class="nav-item"><a class="nav-link" href="settings.html"><i class="fas fa-cog text-dark"></i> Settings</a></li>
                 </ul>
 
             </div>
