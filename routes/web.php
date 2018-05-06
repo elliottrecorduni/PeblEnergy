882f3558878d961e51cc966fbd79392e1445f11e
Elliott <elliottrecord@gmail.com>
2018-05-06 14:17:58 +0100

    Auto stash before merge of "master" and "origin/master"

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,30 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    global $t1;
+    $t1= [];
+
+    $category = \App\DeviceCategory::all()->first();
+
+    $devices = $category->devices()->get();
+
+    foreach($devices as $device){
+        foreach($device->energy_usages as $usages){
+
+            //Combine all data, 1 for monday, 1 for tuesday etc.
+
+            $usagesInRange = $usages->whereBetween('start_time', [\Carbon\Carbon::now()->startOfWeek()->, \Carbon\Carbon::now()->endOfWeek()])->get();
+            foreach ($usagesInRange as $usage){
+                if ($usage->)
+                array_push($t1, [$usage->kw_usage, $usage->start_time, $usage->end_time]);
+            }
+        }
+    }
+
+
+
+    return view('pages.index', compact('t1'));
 });
 
 Auth::routes();
